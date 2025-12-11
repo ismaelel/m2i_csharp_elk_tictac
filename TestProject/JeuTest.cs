5d82908554244db8c8255c4201638e2c6ec68bc2
El Kasbaoui Ismael <ismo67nhf@hotmail.fr>
2025-12-11 16:47:26 +0100

    feat(TD5): Automated Testing

---
+++ TestProject/JeuTest.cs
@@ -0,0 +1,34 @@
+﻿namespace TestProject
+{
+    public class JeuTest
+    {
+        [Fact]
+        public void Scenario_PartieNulle_GrillePleine()
+        {
+            // Arrange
+            Plateau plateau = new Plateau();
+
+            // Act
+
+            //// Ligne 1 : X O X
+            plateau.PlacerCoup(0, 0, 'X');
+            plateau.PlacerCoup(0, 1, 'O');
+            plateau.PlacerCoup(0, 2, 'X');
+
+            //// Ligne 2 : X X O
+            plateau.PlacerCoup(1, 0, 'X');
+            plateau.PlacerCoup(1, 1, 'X');
+            plateau.PlacerCoup(1, 2, 'O');
+
+            //// Ligne 3 : O X O
+            plateau.PlacerCoup(2, 0, 'O');
+            plateau.PlacerCoup(2, 1, 'X');
+            plateau.PlacerCoup(2, 2, 'O');
+
+            char resultat = plateau.VerifierFinDePartie();
+
+            // Assert
+            Assert.Equal('N', resultat); // N = match nul
+        }
+    }
+}
---
+++ TestProject/PlateauTest.cs
@@ -0,0 +1,38 @@
+﻿namespace TestProject
+{
+    public class PlateauTest
+    {
+        [Fact]
+        public void PlacerCoup_CaseVide_True()
+        {
+            // Arrange
+            Plateau plateau = new Plateau();
+            int ligne = 0;
+            int colonne = 0;
+            char joueur = 'X';
+
+            // Act
+            bool coupReussi = plateau.PlacerCoup(ligne, colonne, joueur);
+
+            // Assert
+            Assert.True(coupReussi);
+
+        }
+
+        [Fact]
+        public void PlacerCoup_CasePrise_False()
+        {
+            // Arrange
+            Plateau plateau = new Plateau();
+            plateau.PlacerCoup(0, 0, 'X');
+
+            // Act
+            bool coupReussi = plateau.PlacerCoup(0, 0, '0');
+
+            // Assert
+            Assert.False(coupReussi);
+
+        }
+
+    }
+}
